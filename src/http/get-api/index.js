0a99e7e4ddbd645cef748384e4dc5f04219a3d22
Simon MacDonald <simon.macdonald@gmail.com>
2021-05-16 23:38:47 -0400

    🔒  Validate header and auth token

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -1,8 +1,35 @@
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
-exports.handler = async function http(req) {
-  const twiml = new MessagingResponse();
+const shouldValidate = process.env.NODE_ENV !== "testing";
 
+exports.handler = async function http(req) {
+  // Validate the webhook
+  if (shouldValidate) {
+    if (!req.headers["X-Twilio-Signature"]) {
+      return {
+        headers: {
+          "content-type": "text/plain",
+        },
+        statusCode: 400,
+        body: "No signature header error - X-Twilio-Signature header does not exist, maybe this request is not coming from Twilio.",
+      };
+    }
+    if (!process.env.TWILIO_AUTH_TOKEN) {
+      console.error(
+        "[Twilio]: Error - Twilio auth token is required for webhook request validation."
+      );
+      return {
+        headers: {
+          "content-type": "text/plain",
+        },
+        statusCode: 500,
+        body: "Webhook Error - we attempted to validate this request without first configuring our auth token.",
+      };
+    }
+  }
+
+  // Create the response
+  const twiml = new MessagingResponse();
   twiml.message("The Robots are coming! Head for the hills!");
 
   return {
